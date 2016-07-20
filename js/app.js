947ba69e5a836ec27a5ec2b71e4549d3c4288d73
Chelsea <chelsea@Chelseas-MacBook-Pro.local>
2016-07-19 21:48:10 -0400

    4 bugs on page
    Bug objects are on canvas

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 // Enemies our player must avoid
-var Enemy = function(loc) {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here, we've provided one for you to get started
     // The image/sprite for our enemies, this uses a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
     //var obj = Object.create(Enemy.prototype);
     //obj.loc = loc;
     //return obj;
-    this.x = 202;
-    this.y = 230;
+    this.x = x;
+    this.y = y;
 };
 
 // Update the enemy's position, required method for game
@@ -26,10 +26,6 @@ Enemy.prototype.move = function(){
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
-
-// CS - i think i need to use this somewhere but idk where
-//     this.x = x;
-//     this.y = y;
 
 // Player class with image file path & location for when the game 1st opens
 var Player = function() {
@@ -93,7 +89,7 @@ switch(keyPressed) {
     console.log('no key pressed!');
     break;
     }
-    console.log (this.y);
+    console.log (this.x);
 };
 
 // Now instantiate your objects.
@@ -101,10 +97,10 @@ switch(keyPressed) {
 // Place the player object in a variable called player
 
 var player = new Player();
-var enemy1 = new Enemy();
-var enemy2 = new Enemy();
-var enemy3 = new Enemy();
-var enemy4 = new Enemy();
+var enemy1 = new Enemy(200, 60);
+var enemy2 = new Enemy(200, 145);
+var enemy3 = new Enemy(200, 230);
+var enemy4 = new Enemy(305, 230);
 var allEnemies = [enemy1, enemy2, enemy3, enemy4];
 
 // This listens for key presses and sends the keys to your Player.handleInput() method. You don't need to modify this.
